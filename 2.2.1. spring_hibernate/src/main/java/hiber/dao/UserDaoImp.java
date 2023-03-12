666a6a9b286f4392362aeaafdf1c8752c2bd1f5c
antlau2000 <61512568+antlau2000@users.noreply.github.com>
2023-03-12 19:31:59 +0300

    add Car

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,6 +1,7 @@
 package hiber.dao;
 
 import hiber.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -26,4 +27,12 @@ public class UserDaoImp implements UserDao {
       return query.getResultList();
    }
 
+   @Override
+   public User getUserByCar(String model, int series){
+      Session session = sessionFactory.openSession();
+      return session.createQuery("SELECT u FROM User u INNER JOIN FETCH u.car c WHERE c.model=:m AND c.series=:s", User.class)
+              .setParameter("m",model)
+              .setParameter("s",series)
+              .getSingleResult();
+   }
 }
